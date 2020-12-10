b9a7a1d4c7d648d100ae2e7d695603fb7946a658
Braedon Dickerson <Braedon.Dickerson@necam.com>
2020-12-10 04:32:31 -0500

    feat(stitching): Added multiple image stitching and ROI.
    Stitching and homography computation are now separate
    functionalities.
    
    Added support for stitching multiple image-pairs.
    
    Added support for ROI homography computation.
    
    Color images are now stitched together and displayed.

--- src/ImageStitcher.hpp
+++ src/ImageStitcher.hpp
@@ -8,8 +8,22 @@ public:
     ImageStitcher() {};
     ~ImageStitcher() {};
     
-    bool stitchImages(cv::Mat& src, cv::Mat& dst, cv::Mat& stitchedImg);
+    void setHomography(const cv::Mat& homog);
+    cv::Mat getHomography();
+    bool getHomography(cv::Mat& homog);
+
+    bool computeHomography(const std::pair<cv::Mat, cv::Mat>& imgs,
+                           std::pair<cv::Mat, unsigned int>& homog);
+
+    bool computeHomography(const std::pair<cv::Mat, cv::Mat>& imgs,
+                           float roiWidthPerc,
+                           float roiHeighPerc,
+                           std::pair<cv::Mat, unsigned int>& homog);
+
+    bool stitchImages(const std::pair<cv::Mat, unsigned int>& homog,
+                      const std::vector<std::pair<cv::Mat, cv::Mat>>& imgPairs,
+                      std::vector<cv::Mat>& stitchedImgs);
     
 private:
-    
+    cv::Mat _homography;
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,72 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
+#include <chrono>
+#include <thread>
+
+#include "ImageStitcher.hpp"
+
+void testStitching(const std::string& leftImgPath, const std::string& rightImgPath,
+                   int numIterations, float roiWidthPerc, float roiHeightPerc);
+void printUsage();
 
 int main(int argc, char* argv[])
 {
-    std::cout << "Hello World\n";
+    if (argc != 3) {
+        printUsage();
+        return 0;
+    }
 
+    testStitching(argv[1], argv[2], 4, 0.5, 1.0);
+    
     return 0;
+}
+
+void testStitching(const std::string& leftImgPath, const std::string& rightImgPath,
+                   int numIterations, float roiWidthPerc, float roiHeightPerc)
+{
+    std::vector<std::pair<cv::Mat, cv::Mat>> imgPairs;
+    for (int i = 0; i < 4; i++)
+    {
+        cv::Mat leftImgImg = cv::imread(leftImgPath);
+        cv::Mat dstImg = cv::imread(rightImgPath);
+        imgPairs.push_back(std::pair<cv::Mat, cv::Mat>(leftImgImg, dstImg));
+    }
+
+    std::vector<cv::Mat> stitchedImgs;
+    std::vector<std::pair<cv::Mat, unsigned int>> homogs;
+    ImageStitcher imgStitcher;
+    std::chrono::high_resolution_clock time;
+
+    for (size_t i = 0; i < imgPairs.size(); i++)
+    {
+        printf("Computing homography ROI\n");
+        std::pair<cv::Mat, unsigned int> homography;
+        auto start = time.now();
+        imgStitcher.computeHomography(imgPairs.at(i), roiWidthPerc, roiHeightPerc, homography);
+        auto end = time.now();
+        homogs.push_back(homography);
+        printf("Done computing homography ROI: %lldms\n", std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count());
+
+        printf("Stitching Images\n");
+        std::vector<std::pair<cv::Mat, cv::Mat>> curPair = { imgPairs.at(i) };
+        std::vector<cv::Mat> curStitchedImgs;
+        start = time.now();
+        imgStitcher.stitchImages(homogs.at(i), curPair, curStitchedImgs);
+        end = time.now();
+        printf("Done stitching images: %lldms\n", std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count());
+        stitchedImgs.push_back(curStitchedImgs.front());
+    }
+
+    cv::imshow("Left Image", imgPairs.front().first);
+    cv::imshow("Right Image", imgPairs.front().second);
+    for (int i = 0; i < imgPairs.size(); i++)
+    {
+        cv::imshow("Stitched Image - " + std::to_string(i), stitchedImgs.at(i));
+    }
+
+    cv::waitKey(0);
+}
+
+void printUsage() {
+    printf("ParallelPanorama <left-img-path> <right-img-path>\n");
 }
